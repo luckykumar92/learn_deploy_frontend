0c91182e3f6e5cd83947613764b445d637af8157
Lucky Kumar <lucky.kumar@lpu.in>
2024-07-08 21:53:21 +0530

    chore: Update Vercel configuration for API proxy and headers

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  preview: {
-    host: "0.0.0.0",
+  server: {
     strictPort: true, // Exit if the port is already in use
     port: 4001,
     proxy: {
       "/api": {
-        target: "https://print-saathi-backend-c3f95afc5a9f.herokuapp.com",
+        target: "https://print-saathi-backend-c3f95afc5a9f.herokuapp.com", // Your API server
         changeOrigin: true,
-      },
-    },
-  },
-  server: {
-    // host: "0.0.0.0",
-    proxy: {
-      "/api": {
-        target: "https://print-saathi-backend-c3f95afc5a9f.herokuapp.com",
-        changeOrigin: true,
+        configure: (proxy, options) => {
+          proxy.on("proxyRes", (proxyRes, req, res) => {
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader(
+              "Access-Control-Allow-Methods",
+              "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+            );
+            res.setHeader(
+              "Access-Control-Allow-Headers",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+            );
+          });
+        },
       },
     },
   },
