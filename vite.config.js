23fd57fecf5d38b3026dab6fb30dd9816603cac7
Lucky Kumar <lucky.kumar@lpu.in>
2024-07-08 20:43:49 +0530

    chore: Update proxy configuration to use "/api" instead of "/proxy"

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,23 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  preview: {
+    host: "0.0.0.0",
+    strictPort: true, // Exit if the port is already in use
+    port: 4001,
+    proxy: {
+      "/api": {
+        target: "https://print-saathi-backend-c3f95afc5a9f.herokuapp.com",
+        changeOrigin: true,
+      },
+    },
+  },
   server: {
     // host: "0.0.0.0",
     proxy: {
       "/api": {
         target: "https://print-saathi-backend-c3f95afc5a9f.herokuapp.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
