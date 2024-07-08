41e7926d0f65a022216689a725a6061326ac8cd5
Lucky Kumar <lucky.kumar@lpu.in>
2024-07-08 22:10:15 +0530

    chore: Update Vite configuration to use new API server URL

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,14 @@
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    strictPort: true, // Exit if the port is already in use
-    port: 4001,
+    // host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "https://neartocollege-backend-5984b8127320.herokuapp.com", // Your API server
+        target: "https://neartocollege-backend-5984b8127320.herokuapp.com",
         changeOrigin: true,
-        configure: (proxy, options) => {
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            res.setHeader("Access-Control-Allow-Credentials", "true");
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader(
-              "Access-Control-Allow-Methods",
-              "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-            );
-            res.setHeader(
-              "Access-Control-Allow-Headers",
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-            );
-          });
-        },
       },
     },
   },
